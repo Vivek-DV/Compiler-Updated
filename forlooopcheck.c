f197584bacd19a322af8898e0f0424a9391cdfd0
Vivek D V <149104475+Vivek-DV@users.noreply.github.com>
2025-10-30 19:51:34 +0530

    forlooopcheck.c

--- forlooopcheck.c
+++ forlooopcheck.c
@@ -9,14 +9,12 @@ int main(int argc,char *argv[])
     printf("Enter the filename\n");
     return 1;
   }
-  FILE *fp=fopen(argv[1],"r");
-  char ch,lexeme[100][50],check[30];
+  FILE *fp;
+  char ch,lexeme[100][50],check[50];
+  fp=fopen(argv[1],"r");
   int i=0,j=0,flag=0;
-  if(fp==NULL)
-    printf("Error opening file\n");
-    
-   while((ch=fgetc(fp))!=EOF)
-   {
+  while((ch=fgetc(fp))!=EOF)
+  {
     if(isalnum(ch))
     {
       while(isalnum(ch))
@@ -36,18 +34,23 @@ int main(int argc,char *argv[])
       i++;
       j=0;
     }
-   }
-   
-   printf("Lexemes:\n");
-   for(int k=0;k<i;k++)
-   {
-      printf("%s\n",lexeme[k]);
-   }
-   for(int k=0;k<i;k++)
-   {
+  }
+  printf("Lexemes:\n");
+  for(int k=0;k<i;k++)
+  {
+    printf("%s\n",lexeme[k]);
+  }
+  for(int k=0;k<i;k++)
+  {
     if(strcmp("for",lexeme[k])==0)
     {
-      int c=0,j=k;
+      int c=0,j=k+1;
+      if(strcmp("(",lexeme[k+1])!=0)
+      {
+        flag=1;
+        break;
+      }
+      j++;
       strcpy(check,lexeme[j++]);
       while(strcmp(check,")")!=0)
       {
@@ -62,11 +65,15 @@ int main(int argc,char *argv[])
         flag=1;
       }
     }
-   }
-   if(flag==0)
-   {
-    printf("Syntax corrected\n");
-   }
-   else
+  }
+  if(flag==0)
+  {
+    printf("Syntax correct\n");
+  }
+  else
+  {
     printf("Syntax incorrect\n");
+  }
+  fclose(fp);
+  return 0;
 }
